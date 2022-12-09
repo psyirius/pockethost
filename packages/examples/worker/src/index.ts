943a1b6d36034f37fa36e3c94370ed0cf6721cd2
Ben Allfree <ben@benallfree.com>
2022-12-09 01:17:03 -0800

    wip

--- packages/examples/worker/src/index.ts
+++ packages/examples/worker/src/index.ts
@@ -1,12 +1,4 @@
-// @deno-types="https://cdn.jsdelivr.net/npm/pocketbase@^0.8.0/dist/pocketbase.es.d.ts"
-import { EventSource as EventSourceClass } from 'https://cdn.jsdelivr.net/gh/MierenManz/EventSource@53f3ec9001d1eac19645c2214652a6a7aa3a51cb/mod.ts'
-import Pocketbase from 'https://cdn.jsdelivr.net/npm/pocketbase@^0.8.0'
-declare global {
-  // deno-lint-ignore no-var
-  var EventSource: typeof EventSourceClass
-}
-
-globalThis.EventSource = EventSourceClass
+import { PocketBase } from 'https://cdn.jsdelivr.net/gh/benallfree/pockethost@deno-workers/packages/deno-worker/index.ts'
 
 const POCKETBASE_URL = Deno.env.get('POCKETBASE_URL')
 const ADMIN_LOGIN = Deno.env.get('ADMIN_LOGIN')
@@ -26,7 +18,7 @@ if (!ADMIN_PASSWORD) {
 
 console.log(`Connecting to ${POCKETBASE_URL} with ${ADMIN_LOGIN}`)
 
-const client = new Pocketbase(POCKETBASE_URL)
+const client = new PocketBase(POCKETBASE_URL)
 
 try {
   await client.admins.authWithPassword(ADMIN_LOGIN, ADMIN_PASSWORD)
